6f8d1004817c83578a31ef863f4a896df1d988de
Ali Maher <ali.maher.mohammed@gmail.com>
2020-08-27 06:59:50 +0200

    fawry button add

--- src/components/cart/CartTotals.js
+++ src/components/cart/CartTotals.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import PayPalButton from "./PayPalButton";
 import FawryPayButton from "./FawryPayButton";
 
 function CartTotals({ value }) {
